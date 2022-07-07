bb5dffc8e09c1b89c415b7300856d08bbb611d95
Akinwande Tomisin <tomjames156@gmail.com>
2022-07-07 09:53:24 +0100

    updated js html and css

--- simple_interest.js
+++ simple_interest.js
@@ -1,5 +1,8 @@
-const yearsOptions = document.getElementById("year");
-const monthsOptions = document.getElementById("month");
+const yearsOptions = document.querySelector("select#years");
+const monthsOptions = document.querySelector("select#months");
+const infoBtn = document.querySelector("i.fa-circle-info");
+const principalDef = document.querySelector("p.principal-meaning");
+const timeframeOptions = document.querySelectorAll("div.time > input");
 
 function addOptions(start, stop, receiver, option){
     for(i = start; i < (stop + 1); i++){
@@ -9,3 +12,27 @@ function addOptions(start, stop, receiver, option){
 
 addOptions(1, 100, yearsOptions, "year");
 addOptions(1, 12, monthsOptions, "month");
+
+function resetshownTimeFrameOptions(){
+    let timeframes = ['days', 'months', 'years'];
+    for(let timeframe of timeframes){
+        document.querySelector(`div.${timeframe}`).classList.add("default-hide");
+    }
+}
+
+function showTimeFrameOption(timeframe, receiver){
+    for(let timeframe of timeframeOptions){
+        timeframe.checked = false;
+    }
+    resetshownTimeFrameOptions();
+    document.querySelector(`div.${timeframe}`).classList.remove("default-hide");
+    receiver.checked = true;
+}
+
+infoBtn.addEventListener("mouseover", () => {
+    principalDef.classList.remove("default-hide");
+})
+
+infoBtn.addEventListener("mouseout", () =>{
+    principalDef.classList.add("default-hide");
+})
