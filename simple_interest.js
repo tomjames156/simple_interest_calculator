8fef80c46d5cb54a03f22a8a33bd8d82a10fd042
Akinwande Tomisin <tomjames156@gmail.com>
2022-07-15 06:34:33 +0100

    added calculation function

--- simple_interest.js
+++ simple_interest.js
@@ -51,13 +51,71 @@ interestRate.addEventListener("input", () =>{
     currentRate.innerHTML += `${interestRate.value}%`;
 })
 
-computeInterest.addEventListener("click", () =>{
-    if(document.querySelector("div.active input")){
-        console.log(document.querySelector("div.active input").value);
-    }else if(document.querySelector("div.active select")){
-        console.log(document.querySelector("div.active select").value);
+function getInterestType(){
+    for(let interest_type of document.querySelectorAll("div.interest-type div input")){
+        if(interest_type.checked){
+            return interest_type.value;
+        }
     }
-    console.log(`Rate - ${interestRate.value}`);
-    console.log(`Principal - ${principal.value}`);
-    console.log(currency.value);
+}
+
+function calculateInterest(amount, percentage, time, time_measure, interest_form, currency_type="$"){
+    let interest = 0;
+    let result = 0
+    let rate = parseFloat(percentage/100);
+
+    if((interest_form == "simple") && (time_measure == "years")){
+        interest = parseFloat((amount * percentage * time) / 100);
+        result = parseFloat(amount + interest);
+    }else if((interest_form == "simple") && (time_measure == "months")){
+        interest = parseFloat((amount * percentage * time) / 1200);
+        result = parseFloat(amount + interest);
+    }else if((interest_form == "simple") && (time_measure == "days")){
+        interest = parseFloat((amount * percentage * time) / 36500);
+        result = parseFloat(amount + interest);
+    }else if((interest_form == "compound") && (time_measure == "years")){
+        result = 1 + rate;
+        result = result ** time;
+        result *= amount;
+    }else if((interest_form == "compound") && (time_measure == "months")){
+        result = 1 + (rate/12);
+        result = result ** time;
+        result *= amount;
+    }else if((interest_form == "compound") && (time_measure == "days")){
+        result = 1 + rate;
+        result = result ** time;
+        result *= amount;
+    }
+
+
+    return `${currency_type}${result}`;
+}
+
+computeInterest.addEventListener("click", () =>{
+    let number_of_x = 0;
+    let rate = 0;
+    let principal_amount = 0;
+    let timeframe = "";
+    let currency_symbol = ""; 
+    let interest_type = "";
+
+    rate = parseInt(interestRate.value);
+    principal_amount = parseInt(principal.value);
+    currency_symbol = currency.value;
+    interest_type = getInterestType();
+
+    if(document.querySelector("div.active input")){
+        number_of_x = parseInt(document.querySelector("div.active input").value);
+        timeframe = "days";
+    }else if(document.querySelector("div.active select")){
+        number_of_x = parseInt(document.querySelector("div.active select").value);
+        if(document.querySelector("div.active").classList.contains("months")){
+            timeframe = "months"
+        }else if(document.querySelector("div.active").classList.contains("years")){
+            timeframe = "years";
+        }
+    }
+    
+
+    console.log(calculateInterest(principal_amount, rate, number_of_x, timeframe, interest_type, currency_symbol));
 })
